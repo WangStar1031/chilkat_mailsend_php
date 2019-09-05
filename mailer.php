74436b99100895b86211c625f0b98da6e5baf379
Wang Xing <wangstar1031@hotmail.com>
2019-09-05 18:54:06 +0800

    Send mail with using several proxies.

--- mailer.php
+++ mailer.php
@@ -38,66 +38,99 @@ print $glob->lastErrorText() . "\n";
 
 // The mailman object is used for sending and receiving email.
 $mailman = new CkMailMan();
-$mailman->put_SocksHostname('190.85.19.146');
-$mailman->put_SocksPort(8008);
-$mailman->put_SocksVersion(4);
-
 $mailman->UnlockComponent("30-day trial");
 
+$strRecipient = @file_get_contents("emails.txt");
+$arrBuff = explode(PHP_EOL, $strRecipient);
+$arrRecipients = [];
+foreach ($arrBuff as $value) {
+    if( $value != ""){
+        $arrRecipients[] = $value;
+    }
+}
+$strSocks = @file_get_contents("proxies.txt");
+$arrBuff = explode(PHP_EOL, $strSocks);
+$arrSocks = [];
+foreach ($arrBuff as $value) {
+    if( $value != ""){
+        $arrSocks[] = $value;
+    }
+}
+$sockIndex = 0;
+function setSock(){
+    global $mailman;
+    global $arrSocks;
+    global $sockIndex;
+    $curSock = $arrSocks[$sockIndex++];
+    $sockIndex = $sockIndex >= count($arrSocks) ? $sockIndex - count($arrSocks) : $sockIndex;
+    $arrBuff = explode(" ", $curSock);
+    $mailman->put_SocksHostname($arrBuff[0]);
+    $mailman->put_SocksPort($arrBuff[1]);
+    $mailman->put_SocksVersion($arrBuff[2]);
+    // $mailman->put_SocksHostname('190.85.19.146');
+    // $mailman->put_SocksPort(8008);
+    // $mailman->put_SocksVersion(4);
+}
+
+setSock();
 //$recipient = '7602241960@txt.att.net';
-$recipient = 'locojefe1337@gmail.com';
+// $recipient = 'locojefe1337@gmail.com';
+function sendEmail($recipient){
+    global $mailman;
+    $smtpHostname = $mailman->mxLookup($recipient);
+    if ($mailman->get_LastMethodSuccess() != 1) {
+        print $mailman->lastErrorText() . "\r\n";
+    }
 
-#  Do a DNS MX lookup for the recipient's mail server.
+    #  Set the SMTP server.
+    $mailman->put_SmtpHost($smtpHostname);
 
-$smtpHostname = $mailman->mxLookup($recipient);
-if ($mailman->get_LastMethodSuccess() != 1) {
-print $mailman->lastErrorText() . "\r\n";
-print $pass;
+    // Create a new email object
+    $email = new CkEmail();
+
+    $email->put_Subject('This is a test');
+    $email->put_Body('This is a test');
+    $email->put_FromName('RASTA');
+    $email->put_FromAddress('ahsdhsadhsah@alpha.com');
+    $email->AddTo("",$recipient);
+    $email->put_Charset('UTF-8');
+    $email->put_Mailer('Mailer');
+    $email->AddHeaderField('Content-Transfer-Encoding','base64');
+    $success = $mailman->SendEmail($email);
+
+    $smtpHostname = $mailman->mxLookup($recipient);
+    if ($mailman->get_LastMethodSuccess() != 1) {
+        print $mailman->lastErrorText() . "\r\n";
+    }
+
+    if ($success != true) {
+        print $mailman->lastErrorText() . "\n";
+        print 'ConnectFailReason = ' . $mailman->get_ConnectFailReason() . "\n";
+        return false;
+    }
+
+    $success = $mailman->SmtpAuthenticate();
+    if ($success != true) {
+        print $mailman->lastErrorText() . "\n";
+        return false;
+    }
+
+    $success = $mailman->SendEmail($email);
+    if ($success != true) {
+        print $mailman->lastErrorText() . "\n";
+        return false;
+    }
+
+    $mailman->CloseSmtpConnection();
+
+    print 'Email Sent.' . "\n";
+    return true;
 }
-
-#  Set the SMTP server.
-$mailman->put_SmtpHost($smtpHostname);
-
-
-// Create a new email object
-$email = new CkEmail();
-
-$email->put_Subject('This is a test');
-$email->put_Body('This is a test');
-$email->put_FromName('RASTA');
-$email->put_FromAddress('ahsdhsadhsah@alpha.com');
-$email->AddTo("",$recipient);
-$email->put_Charset('UTF-8');
-$email->put_Mailer('Mailer');
-$email->AddHeaderField('Content-Transfer-Encoding','base64');
-$success = $mailman->SendEmail($email);
-
-$smtpHostname = $mailman->mxLookup($recipient);
-if ($mailman->get_LastMethodSuccess() != 1) {
-print $mailman->lastErrorText() . "\r\n";
-print $pass;
+foreach ($arrRecipients as $recipient) {
+    print $recipient . "\n";
+    while( !sendEmail($recipient)){
+        setSock();
+    }
 }
-
-if ($success != true) {
-    print $mailman->lastErrorText() . "\n";
-    print 'ConnectFailReason = ' . $mailman->get_ConnectFailReason() . "\n";
-    exit;
-}
-
-$success = $mailman->SmtpAuthenticate();
-if ($success != true) {
-    print $mailman->lastErrorText() . "\n";
-    exit;
-}
-
-$success = $mailman->SendEmail($email);
-if ($success != true) {
-    print $mailman->lastErrorText() . "\n";
-    exit;
-}
-
-$mailman->CloseSmtpConnection();
-
-print 'Email Sent.' . "\n";
 
 ?>
